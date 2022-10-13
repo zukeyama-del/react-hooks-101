7324e3c06fd5bc65cfc4bdf983e64492ab715392
zukeyama <zukeyama_shuto@beanslabo.co.jp>
2022-10-13 14:12:37 +0900

    implement oject state

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
 import React, { useState } from 'react'
 
 const App = (props) => {
-  const [name, setName] = useState(props.name)
-  const [price, setPrice] = useState(props.price)
+  const [state, setState] = useState(props)
+  const {name, price} = state
 
 
-  const reset = () => {
-    setPrice(props.price)
-    setName(props.name)
-  }
   return (
     <>
       <p>現在の{name}は、{price}円です。</p>
-      <button onClick={() => setPrice(price + 1)}>+1</button>
-      <button onClick={() => setPrice(price - 1)}>-1</button>
-      <button onClick={reset}>Reset</button>
-      <input value={name} onChange={e => setName(e.target.value)} ></input>
+      <button onClick={() => setState({...state, price: price + 1})}>+1</button>
+      <button onClick={() =>setState({...state, price: price - 1})}>-1</button>
+      <button onClick={() => {setState(props)}}>Reset</button>
+      <input value={name} onChange={e => setState({...state, name: e.target.value})} ></input>
     </>
   )
 }
 
 App.defaultProps = {
-  name: 'サンプル',
+  name: '',
   price: 1000
 }
 
