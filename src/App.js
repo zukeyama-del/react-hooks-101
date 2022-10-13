6f398b336aeeb8ef6529da66cee4ce17ef8b302c
zukeyama <zukeyama_shuto@beanslabo.co.jp>
2022-10-13 11:32:30 +0900

    my first React Hook with useState

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const App = () => {
+  const [count, setCount] = useState(0)
+
+  const countUp = () =>{
+    setCount(count + 1)
+  }
+  const countDown = () => {
+    setCount(count - 1)
+  }
+
+  const countUp2 = () =>{
+    setCount(previousCount => previousCount + 1)
+  }
+  const countDown2 = () => {
+    setCount(previousCount => previousCount - 1)
+  }
+  
+  const reset = () => setCount(0)
+
+  const twoTimes = () => setCount(previousCount => previousCount * 2)
+
+  const threeDivide = () => {
+    if(count % 3 === 0){
+      setCount(count / 3)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div>count: {count}</div>
+      <br/>
+      <button onClick={countUp}>カウントアップ</button>
+      <br/>
+      <button onClick={countDown}>カウントダウン</button>
+      <br/>
+      <button onClick={countUp2}>カウントアップ2</button>
+      <br/>
+      <button onClick={countDown2}>カウントダウン2</button>
+      <button onClick={reset}>リセット</button>
+      <button onClick={twoTimes}>×2</button>
+      <button onClick={threeDivide}>3の倍数の時だけ3で割る</button>
+    </>
+  )
 }
 
-export default App;
+export default App
