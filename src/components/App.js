64877c3aba3dead6d16f467955716aaaf9a52e02
zukeyama <zukeyama_shuto@beanslabo.co.jp>
2022-10-13 16:20:07 +0900

    create event form and event table with bootstrap

---
+++ src/components/App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <>
+    <div className="container-fluid">
+      <h4>イベント作成フォーム</h4>
+      <form>
+        <div className="form-group">
+          <label htmlFor="formEventTitle">タイトル</label>
+          <input className='form-control' id="formEventTitle"/>
+          </div>
+
+          <div className="form-group">
+          <label htmlFor="formEventBody">ボディー</label>
+          <textarea className='form-control' id="formEventBody"/>
+          </div>
+
+          <button className='btn btn-primary'>イベントを作成する</button>
+          <button class="btn btn-danger">全てのイベントを削除する</button>
+      </form>
+
+      <h4>イベント一覧</h4>
+      <table class="table table-hover">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>タイトル</th>
+            <th>ボディー</th>
+            <th></th>
+            </tr>
+          <tbody>
+          </tbody>
+        </thead>
+      </table>
+    </div>
+    </>
+  );
+}
+
+export default App;
